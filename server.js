3afae1e889a2f9e80391cd45ef89e0de93cf8e31
Yi-Ting Chiu <mytim710@gmail.com>
2022-11-04 16:18:53 -0700

    database created

---
+++ server.js
@@ -0,0 +1,53 @@
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+const express = require('express')
+const app = express()
+const { engine } = require('express-handlebars')
+const cookieParser = require('cookie-parser')
+
+const port = 8080
+
+// use handlebars as the view engine: what process the dynamic html
+// app.engine('handlebars', engine())
+// app.set('view engine', "handlebars");
+// app.set("views", "./views")
+
+// app.use(express.static('static'));
+// app.use(express.urlencoded( {extended: false}));
+// app.use(cookieParser());
+
+const dbPromise = sqlite.open({
+    filename: './database/sakila.sqlite',
+    driver: sqlite3.Database
+})
+
+app.get('/', (req, res) => {
+
+    res.render('home') 
+    //render 會告訴view engine 這是獨立的一個頁面，不需要layout
+})
+
+app.post("/get_actor", async (req, res) => {
+    const lastName = req.body.actor;
+    const db = await dbPromise;
+    let query = 'SELECT * FROM actor WHERE last_name = ?';
+    let result = await db.get(query, lastName);
+
+    if(result)
+    {
+        console.log(result)
+    } else {
+        console.log("No result found")
+    }
+
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
+
+
+// handlebar 是一個template engine，可以讓我們在html裡面加入一些動態的內容
+// 例如：{{}}，這個就是handlebar的語法，可以在裡面放入一些變數，或是function
+// 這邊我們放入一個變數，變數的內容是從server.js傳過來的
+// Mustache Template Engine
